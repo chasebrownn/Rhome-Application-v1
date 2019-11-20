811e724f3fe1f9e9ad6672d745eca6678d9ba021
Chase Brown <chasebrown@Chases-MacBook-Pro-2.local>
2019-11-20 16:47:07 -0700

    Add

---
+++ Rhome Application v1/Rhome Application v1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Rhome Application v1
+//
+//  Created by Chase Brown on 11/20/19.
+//  Copyright © 2019 Chase Brown. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Ben!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Rhome Application v1/Rhome Application v1Tests/Rhome_Application_v1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Rhome_Application_v1Tests.swift
+//  Rhome Application v1Tests
+//
+//  Created by Chase Brown on 11/20/19.
+//  Copyright © 2019 Chase Brown. All rights reserved.
+//
+
+import XCTest
+@testable import Rhome_Application_v1
+
+class Rhome_Application_v1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
