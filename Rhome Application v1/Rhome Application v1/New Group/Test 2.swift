f2a6d27f24e60a50498fa1b0b23ab73066a48fb2
Ben Dixon <BenDixon@Bens-MacBook-Pro.local>
2019-11-20 17:52:53 -0700

    edit

---
+++ Rhome Application v1/Rhome Application v1/New Group/Test 2.swift
@@ -0,0 +1,25 @@
+//
+//  Test 2.swift
+//  Rhome Application v1
+//
+//  Created by Ben Dixon on 11/20/19.
+//  Copyright © 2019 Chase Brown. All rights reserved.
+//
+
+import SwiftUI
+
+struct Test_2: View {
+   
+    var body: some View {
+        VStack{
+       
+        Text("Chase is cool")
+        }
+    }
+}
+
+struct Test_2_Previews: PreviewProvider {
+    static var previews: some View {
+        Test_2()
+    }
+}
