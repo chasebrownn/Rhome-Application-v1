3a6b72e9b8bc6c783906c293203bb3a09fb2cbb5
Ben Dixon <BenDixon@Bens-MacBook-Pro.local>
2019-11-20 18:02:59 -0700

    Edit2

---
+++ Rhome Application v1/Rhome Application v1/New Group/Test 3.swift
@@ -0,0 +1,21 @@
+//
+//  Test 3.swift
+//  Rhome Application v1
+//
+//  Created by Ben Dixon on 11/20/19.
+//  Copyright © 2019 Chase Brown. All rights reserved.
+//
+
+import SwiftUI
+
+struct Test_3: View {
+    var body: some View {
+        Text("Dark Lord Rise")
+    }
+}
+
+struct Test_3_Previews: PreviewProvider {
+    static var previews: some View {
+        Test_3()
+    }
+}
